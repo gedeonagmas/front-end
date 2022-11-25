8e0ff97b18f2c3b490b7eac8452b8acd5a286a0a
gedeonagmas <gedeonagmas2580@gmail.com>
2022-11-25 04:14:08 +0300

    update api slice

--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const apiSlice = createApi({
   reducerPath: "api",
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:2200" }),
+  baseQuery: fetchBaseQuery({ baseUrl: "https://iot-back-end.onrender.com" }),
   tagTypes: [
     "acceptedProjects",
     "newProjects",
